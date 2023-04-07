9fe8c0f1bbc74ff52b6dc7111c7d73e8b19f6739
daffaresyaly <daffaresyaly@gmail.com>
2023-04-07 17:30:41 +0700

    added layout components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import Header from '@/components/Header';
+
 export default function Home() {
-  return <div className="text-3xl text-sky-500">Hello World</div>;
+  return <Header label="Home" />;
 }
